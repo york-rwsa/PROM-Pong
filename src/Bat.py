8a129f3ff4ef5cded3701eb305c38bbf892a632f
RWS Anderson <rwsa500@cse168pc-32.cs.york.ac.uk>
2019-05-01 17:34:51 +0100

    add size increase functionality

--- src/Bat.py
+++ src/Bat.py
@@ -1,21 +1,29 @@
 import constants
+import datetime
 from GameObject import GameObject
 
 
 class Bat(GameObject):
     def __init__(self, x, starty, speed, colour, controller, width=1, height=3):
         super().__init__(x, starty, width, height, colour)
-        self.speed = speed
+        self.speed = constants.NUMBER_OF_SIZE_INCREASES
         self.controller = controller
 
+        self.sizeIncreasedAt = None
+        self.sizeIncreasesLeft = constants.NUMBER_OF_SIZE_INCREASES
+
     def update(self, timeDiff):
-        # controller logic would go here
-        # just go up and down in one spot
-        controllerPos = self.controller.getPositionPercent() * (24 - self.size.y)
+        self.checkSize()
+        self.updatePos(self.controller.getPositionPercent() * (24 - self.size.y))
 
-        # if round(controllerPos) == self.pos.y:
-        #     return
+        if self.controller.getTopButton():
+            self.increaseSize()
+            print('increased Size')
 
+        if self.controller.getBottomButton():
+            print('bottom')
+
+    def updatePos(self, controllerPos):
         if controllerPos > self.pos.y:
             self.pos.y += min(self.speed, controllerPos - self.pos.y)
         elif controllerPos < self.pos.y:
@@ -26,7 +34,21 @@ class Bat(GameObject):
         elif self.pos.y <= 0:
             self.pos.y = 0
 
-        if self.controller.getTopButton():
-            print('top')
-        if self.controller.getBottomButton():
-            print('bottom')
+    def checkSize(self):
+        if self.sizeIncreasedAt == None:
+            return
+
+        timeDiff = datetime.datetime.now() - self.sizeIncreasedAt
+
+        if timeDiff.total_seconds() > constants.SIZE_INCREASE_TIME:
+            self.size.y = int(self.size.y / constants.INCREASE_FACTOR)
+            self.sizeIncreasedAt = None
+
+    def increaseSize(self):
+        if self.sizeIncreasedAt != None or self.sizeIncreasesLeft == 0:
+            return
+
+        self.size.y = int(self.size.y * constants.INCREASE_FACTOR)
+
+        self.sizeIncreasedAt = datetime.datetime.now()
+        self.sizeIncreasesLeft -= 1
