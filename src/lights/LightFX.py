a46a41f04a5015d6bf9412b6852f1501aea4283c
SecureCake <44171454+SecureCake@users.noreply.github.com>
2019-05-06 17:05:42 +0100

    Cleaned code

--- src/lights/LightFX.py
+++ src/lights/LightFX.py
@@ -1,41 +1,21 @@
 from PyGlow import PyGlow
 from time import sleep
+
 pyglow = PyGlow(speed = 1000) #max brightness 255
 pyglow.all(0) #reset LEDs
+
 brightness = 150
 speed = 10
 speed2 = 5
+colors = ["white", "blue", "green", "yellow", "orange", "red"]
 
 def pointWon():
-    for i in range(0, brightness, speed):
-        pyglow.color("white", i)
-    for i in range(0, brightness, speed):
-        pyglow.color("blue", i)
-    for i in range(0, brightness, speed):
-            pyglow.color("green", i)
-    for i in range(0, brightness, speed):
-        pyglow.color("yellow", i)
-    for i in range(0, brightness, speed):
-        pyglow.color("orange", i)
-    for i in range(0, brightness, speed):
-        pyglow.color("red", i)
-    sleep(.2)
-    for i in range(brightness, -1, -speed2):
-        pyglow.color("white", i)
-    for i in range(brightness, -1, -speed2):
-        pyglow.color("blue", i)
-    for i in range(brightness, -1, -speed2):
-            pyglow.color("green", i)
-    for i in range(brightness, -1, -speed2):
-        pyglow.color("yellow", i)
-    for i in range(brightness, -1, -speed2):
-        pyglow.color("orange", i)
-    for i in range(brightness, -1, -speed2):
-        pyglow.color("red", i)
-
-
-
-
+    for c in colors:
+        for i in range(0, brightness, speed):
+            pyglow.color(c, i)
+    for c in colors:
+         for i in range(brightness, -1, -speed2):
+        pyglow.color(c, i)
 
 '''
 RED = [1,7,13]
