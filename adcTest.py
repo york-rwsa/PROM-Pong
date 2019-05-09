64f862fec027e59309758549a197967e6cae6dee
RWS Anderson <rwsa500@cse168pc-29.cs.york.ac.uk>
2019-05-09 13:05:05 +0100

    add cpld adc

--- adcTest.py
+++ adcTest.py
@@ -1,11 +1,14 @@
 import smbus
 import time
 
-i2cAddr = 0x21
-code = 0x10
+i2cAddr = 0x3a
 
 bus = smbus.SMBus(1)
-bus.write_byte(i2cAddr, code)
-tmp = bus.read_word_data(i2cAddr, 0x00)
-print("{0:b}".format(tmp))
-print(tmp)
+bus.write_byte(i2cAddr, 0xFF)
+
+
+while True:
+    tmp = bus.read_byte(i2cAddr) & 0x7F
+    print("{0:b}".format(tmp))
+    print(tmp)
+    time.sleep(1)
